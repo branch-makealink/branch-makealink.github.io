9430363e2d0e47842b90b0cad481a39104676876
Mike Horn <mhorn@branch.io>
2020-07-16 18:24:15 -0400

    fix rebase merge conflicts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
 // Ads Link Creation
 
 // Email Link Creation
+=======
+console.log('This would be the main JS file.');
+$(document).ready(function(){  
+	var selection = document.querySelector('input[name="quicklinkradios"]:checked').value;
+	console.log(selection);	
+	document.getElementsByClassName("key-value-form-container")[0].style.display = 'none';
+}) 
+
+function quickLinkTypeSelection() {
+  var selection = document.querySelector('input[name="quicklinkradios"]:checked').value;
+  console.log(selection);	
+}
+
+function quickLinkNext() {
+  var selection = document.querySelector('input[name="quicklinkradios"]:checked').value;
+  console.log(selection);
+  if (selection == "App") {
+  	window.location = "file:///Users/mhorn/Desktop/branch-makealink.github.io/quick-links-2.html";
+  }
+
+  if (selection == "Mobile Web") {
+  	window.location = "file:///Users/mhorn/Desktop/branch-makealink.github.io/quick-links-3.html";
+  }
+}
+
+function hideForm(){
+  document.getElementsByClassName("key-value-form-container")[0].style.display = 'none';
+}
+function showForm(){
+    document.getElementsByClassName("key-value-form-container")[0].style.display = 'block';
+}
+>>>>>>> remove useless code
