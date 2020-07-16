7e1cd0ef8d19b513551c50c0b790ff1715ea184c
Jon Shek <jon.shek@branch.io>
2020-07-16 09:16:45 -0700

    first draft of email

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+// Ads Link Creation
+
+// Email Link Creation
+function emailLinkCreator() {
+	var content = document.getElementById('emailBox');
+
+	console.log(content.style.display)
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+};
