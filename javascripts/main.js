4b53a4fd277edd4d343de254088f8cf1029d9a09
Jon Shek <jon.shek@branch.io>
2020-07-16 13:01:52 -0700

    radio buttons almost finished

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,3 @@
 // Ads Link Creation
 
 // Email Link Creation
-function emailButtonToggle() {
-	var yes = document.getElementById("emailYes");
-    
-    if (yes.checked == false) {
-        yes.checked = true;
-    } else {
-    	yes.checked = false;
-    }
-};
