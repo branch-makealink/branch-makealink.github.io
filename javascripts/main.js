abcbd0c9ad9bf59d368bfc5ed6d803e351d70995
Jon Shek <jon.shek@branch.io>
2020-07-16 18:58:37 -0700

    finished UI for first 5 pages. updated next button function to apply to all pages

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,25 +6,42 @@ $(document).ready(function(){
 }) 
 
 function quickLinkTypeSelection() {
-  var selection = document.querySelector('input[name="quicklinkradios"]:checked').value;
-  console.log(selection);	
-}
+  var selection = document.querySelector('input[name="initRadios"]:checked').value;
+  console.log(selection);
 
+  if (selection == "App") {
+  	document.querySelector('#initPageButton').innerHTML = "Build Deep Link";
+  }	else {
+  	document.querySelector('#initPageButton').innerHTML = "Build Web Only Link";
+  }
+};
+
+
+// saving this in case we plan to send user ahead a few pages 
 function quickLinkNext() {
   var selection = document.querySelector('input[name="quicklinkradios"]:checked').value;
   console.log(selection);
-  if (selection == "App") {
-  	window.location = "quick-links-2.html";
+
+  page = parseInt(document.location.href.slice(-6,-5),10);
+
+  if (selection == "App" || selection == "Mobile Web") {
+  	page += 1;
+  	window.location = "quick-links-"+ page +".html";
   }
 
-  if (selection == "Mobile Web") {
-  	window.location = "quick-links-3.html";
-  }
+}
+
+function next() {
+	page = parseInt(document.location.href.slice(-6,-5),10);
+	page += 1;
+
+	window.location = "quick-links-" + page + ".html";
 }
 
 function hideForm(){
   document.getElementsByClassName("key-value-form-container")[0].style.display = 'none';
 }
+
 function showForm(){
     document.getElementsByClassName("key-value-form-container")[0].style.display = 'block';
 }
